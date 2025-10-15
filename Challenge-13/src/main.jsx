f24180e01bacef0eb0dce6d85b6112c9f0251b5a
Juano-LP <juanchomontealeg@gmail.com>
2025-10-15 10:54:31 -0500

    Reto 13  Firebase 50%

---
+++ Challenge-13/src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Registro } from "./components/registro.jsx";
+import { Login } from "./components/login.jsx";
+import { Chat } from "./components/chat.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
