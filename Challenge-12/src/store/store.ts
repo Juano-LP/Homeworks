4cb29ad529b5e066d99240a8cdb91d9ad707d0dc
Juano-LP <juanchomontealeg@gmail.com>
2025-10-15 13:44:20 -0500

    Reto 12 100%

--- Challenge-12/src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import firebaseReducer from "./slices/FirebaseSlice";
-
-export const store = configureStore({
-  reducer: {
-    firebase: firebaseReducer,
-  },
-});
